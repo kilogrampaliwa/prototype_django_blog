6f0dca3ab6e1652fca423f626fdcc4dd2bd7aa21
Filip Ciąder <filip.ciader@gmail.com>
2024-06-03 22:17:22 +0200

    Ended 1st video, all ok.

--- distribution/src/blog/urls.py
+++ distribution/src/blog/urls.py
@@ -4,12 +4,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 
-from .views import index, blog
+from .views import index, blog, post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('blog/', blog),
+    path('post/', post),
 ]
 
 if settings.DEBUG:
